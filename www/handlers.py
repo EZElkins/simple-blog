ff670e7ffabe397b24e7d8c3dd56552947113cf2
elkins <elkins9267@126.com>
2017-05-27 16:11:59 +0800

    1.添加uikit CSS框架 2.使用uikit进行首页的搭建

--- www/handlers.py
+++ www/handlers.py
@@ -3,16 +3,30 @@
 
 # ' url handlers '
 
-import asyncio
+import asyncio, time
 
 from coroweb import get, post
-from models import User
+from models import User, Blog
 
-@get('/')
-async def index(request):
+@get('/users')
+async def users(request):
     users = await User.findAll()
     # print(users)
     return {
         '__template__': 'index.html',
         'users': users
+    }
+
+
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
