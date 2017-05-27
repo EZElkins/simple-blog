f9baf5d49bcc2b85d61505eac72623266dbe110e
elkins <elkins9267@126.com>
2017-05-27 14:13:50 +0800

    add web framework and config file.

---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+
+'''
+Default configurations.
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'robot',
+        'password': '123456',
+        'database': 'simple_blog'
+    },
+    'session': {
+        'secret': 'Aa135246'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+
+'''
+Override configurations.
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
